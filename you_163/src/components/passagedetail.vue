bf33c41376efa8d087afc7dd33ad97b7a59cc5b2
201250104 <2681299706@qq.com>
2022-03-12 23:41:57 +0800

    update for passagedetail

--- you_163/src/components/passagedetail.vue
+++ you_163/src/components/passagedetail.vue
@@ -5,11 +5,13 @@
       <router-link to="/">
         < 返回
       </router-link>
-      <h1 class="hugetitle">第{{this.$route.params.id}}篇</h1>
+      <h1 class="hugetitle">第{{articleId}}篇</h1>
     </div>
 
     <div>
-      {{articles}}
+      {{article.content}}
+      {{article.author}}
+      {{article.title}}
     </div>
 
   </div>
@@ -23,14 +25,20 @@ export default {
   data(){
     return{
       // id:{{this.$route.params}},
-      articles:[]
+      article:[],
+      articleId:this.$route.params.id
     }
   },
   created() {
-    axios.get('http://localhost:8181/article/all').then(_d => {
-      this.articles = _d.data;
+    console.log(this.articleId)
+    console.log("被触发")
+    var articleurl='http://localhost:8181/article/'+this.articleId
+    // console.log(this.articleurl)
+
+    axios.get(articleurl).then(_d => {
+      this.article = _d.data;
     })
-  }
+  },
 }
 </script>
 
